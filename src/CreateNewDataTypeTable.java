d91acd63432c894a1dd7a764fd94bb75ba3dc362
lisa <lisssalavo@gmail.com>
2018-04-06 16:15:08 +0300

    парсинг

---
+++ src/CreateNewDataTypeTable.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.io.IOException;
+
+public class CreateNewDataTypeTable extends JFrame {
+    private Object[] columnsHeader = new String[] {"COLUMN_1"};
+    public CreateNewDataTypeTable() throws IOException{
+        super("ARXML-viewer");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Object[][] array = new Object[1][50];
+
+        JTable table1 = new JTable(array, columnsHeader);
+        /* Размещение таблиц в панели с блочным расположением */
+        Box contents = new Box(BoxLayout.Y_AXIS);
+        contents.add(new JScrollPane(table1));
+        /* Вывод окна на экран */
+        setContentPane(contents);
+        setSize(500, 400);
+        setVisible(true);
+    }
+
+}
--- src/fileopenning/openFile.java
+++ src/fileopenning/openFile.java
@@ -1,3 +1,5 @@
+package fileopenning;
+
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
